8caeb46beef09145e31392af33d94d54bbe71562
Eric Milford <ericmilford@gmail.com>
2012-10-05 13:04:07 -0400

    Initial commit

---
+++ lib/nv-d3js-rails.rb
@@ -0,0 +1,9 @@
+require "nv-d3js-rails/version"
+
+module Nv
+  module D3js
+    module Rails
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/nv-d3js-rails/version.rb
@@ -0,0 +1,7 @@
+module Nv
+  module D3js
+    module Rails
+      VERSION = "0.0.1"
+    end
+  end
+end
